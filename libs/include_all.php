ba8f50ea25c897c9f43795de7dfb407d0c57fe72
Patrik Johansson <patrik@ptjwebben.se>
2015-12-03 10:23:27 +0100

    Fixed easy import of all libraries, should not be used in production but convenient during dev. include_all.php also contains links to doc/examples for the projects

---
+++ libs/include_all.php
@@ -0,0 +1,20 @@
+<?php
+
+	//https://51degrees.com/support/documentation/php
+	require_once dirname(__FILE__).'/51Degrees/core/51Degrees.php';
+	
+	//http://raveren.github.io/kint/
+	require_once dirname(__FILE__).'/kint/Kint.class.php';
+	
+	//http://popeen.com/docs/PHPMailer/
+	require_once dirname(__FILE__).'/PHPMailer/PHPMailerAutoload.php';
+	
+	//https://github.com/php-curl-class/php-curl-class/tree/master/examples
+	require_once dirname(__FILE__).'/php-curl-class/src/Curl/CaseInsensitiveArray.php';
+	require_once dirname(__FILE__).'/php-curl-class/src/Curl/Curl.php';
+	require_once dirname(__FILE__).'/php-curl-class/src/Curl/MultiCurl.php';
+	
+	//http://popeen.com/docs/Kakadua%20PHP%20Snippets/
+	require_once dirname(__FILE__).'/PHP-Snippets/include_functions.php';
+
+?>
